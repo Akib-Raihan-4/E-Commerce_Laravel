d359b4a55b35aeef0c135a10ea2562918dda7ad6
Akib-Raihan-4 <raihanakib062@gmail.com>
2024-03-28 15:32:31 +0600

    Storing Categories in db

--- app/Http/Controllers/admin/CategoryController.php
+++ app/Http/Controllers/admin/CategoryController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
+use App\Models\Category;
 
 class CategoryController extends Controller
 {
@@ -16,6 +17,7 @@ class CategoryController extends Controller
         return view('admin.category.create');
     }
 
+
     public function store(Request $request){
         $validator = Validator::make($request->all(),[
             'name' => 'required',
@@ -24,7 +26,19 @@ class CategoryController extends Controller
         ]);
 
         if ($validator->passes()){
-            
+            $category = new Category();
+            $category->name = $request->name;
+            $category->slug = $request->slug;
+            $category->status = $request->status;
+            $category->save();
+
+            // $request->session()->flash('success','Category added successfully');
+
+            return response()->json([
+                'status' => true,
+                'message' => 'Category added successfully'
+            ]);
+
         }else{
             return response()->json([
                 'status' => false,
